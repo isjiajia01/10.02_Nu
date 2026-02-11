441335aaa684349df8acddd2f834e871cef012d8
Jiajia Zhang <isaaronzhang@icloud.com>
2026-02-11 08:47:56 +0100

    Initial Commit

---
+++ Nu/NuApp.swift
@@ -0,0 +1,17 @@
+//
+//  NuApp.swift
+//  Nu
+//
+//  Created by Zhang Jiajia on 11/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct NuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
